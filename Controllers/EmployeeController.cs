b2489c4f176521e72ecbddb0e9fa954533eb7b35
shrivalli04 <shrivalli04@gmail.com>
2022-09-28 08:14:01 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticDataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public static List<Employee> emps = Employee.getEmployees();
+
+        [HttpGet]
+        public async Task<ActionResult> getAllEmps()
+        {
+            return Ok(emps);
+        }
+
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticDataAPI
+{
+    public class Employee
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public float Salary { get; set; }
+
+
+        public Employee() { }
+
+        public Employee(int eid, string ename,float sal)
+        {
+            Eid = eid;
+            Ename = ename;
+            Salary = sal;
+
+        }
+
+        public static List<Employee> employees = new List<Employee>();
+
+        public static List<Employee> getEmployees()
+        {
+            employees.Add(new Employee(1, "Ram", 85000));
+            employees.Add(new Employee(2, "Sreedhar", 54000));
+            employees.Add(new Employee(3, "Vikram", 74000));
+            employees.Add(new Employee(4, "Sandhya", 7900));
+            return employees;
+        }
+    }
+}
